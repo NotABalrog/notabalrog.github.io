84ee47dfc62db90f5ccb3ecb54a992a93d988260
NotABalrog <brian.michael@enterpriseinnovations.net>
2013-12-19 16:00:00 -0600

    First commit
    Github

---
+++ application.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".bc").hover(function () {
+        $(".defaultHeader").toggle();
+        $(".hidden.bc").toggle();
+    });
+    $(".hr").hover(function () {
+        $(".defaultHeader").toggle();
+        $(".hidden.hr").toggle();
+    });
+    $(".cf").hover(function () {
+        $(".defaultHeader").toggle();
+        $(".hidden.cf").toggle();
+    });
+});
